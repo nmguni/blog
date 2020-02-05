5e6777bdd17506e5a0d33608da6a06bb51890c4f
Ndumiso Mguni <Ndumiso29@gmail.com>
2020-02-04 20:38:33 -0500

    UI work

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,19 @@
-import React from 'react'
+import React from "react"
+import Footer from "../components/footer"
+import Header from "../components/header"
+import "../styles/index.scss"
+import layoutStyles from "./layout.module.scss"
 
-import Footer from '../components/footer'
-import Header from '../components/header'
-import '../styles/index.scss'
-import layoutStyles from './layout.module.scss'
-
-const Layout = (props) => {
-    return (
-        <div className={layoutStyles.container}>
-            <div className={layoutStyles.content} >
-                <Header />
-                {props.children}
-            </div>
-            <Footer />
-        </div>
-    )
+const Layout = props => {
+  return (
+    <div>
+      <Header />
+      <div className={layoutStyles.container}>
+        <div className={layoutStyles.content}>{props.children}</div>
+        <Footer />
+      </div>
+    </div>
+  )
 }
 
 export default Layout
